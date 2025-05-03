b3710884b2f78d6986fedb62ff848aac74e3f25a
Umar Khan <khanumar0108@gmail.com>
2025-05-03 19:48:21 +0530

    feat: Add github copilot plugin

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,6 +74,11 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  ["copilot.vim"] = {
+    loaded = true,
+    path = "/home/umarkhan/.local/share/nvim/site/pack/packer/start/copilot.vim",
+    url = "https://github.com/github/copilot.vim"
+  },
   harpoon = {
     loaded = true,
     path = "/home/umarkhan/.local/share/nvim/site/pack/packer/start/harpoon",
